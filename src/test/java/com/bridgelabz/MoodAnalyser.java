aedc694ef8db60e0736958dffc39831f5d3998e5
sagarchavan07 <chavansagar96044@gmail.com>
2022-08-04 18:24:15 +0530

    given message should respond mood

--- src/test/java/com/bridgelabz/MoodAnalyser.java
+++ src/test/java/com/bridgelabz/MoodAnalyser.java
@@ -1,8 +1,17 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
-    public String analyseMood(String message) {
-        if (message.contains("sad")){
+    String message;
+
+    public MoodAnalyser() {
+    }
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
+        if (this.message.contains("sad")){
             return "SAD";
         }else
             return "HAPPY";
