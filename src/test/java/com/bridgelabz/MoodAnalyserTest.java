9f22072c176928c93e6a2b476e832da36116453e
sagarchavan07 <chavansagar96044@gmail.com>
2022-08-04 18:04:04 +0530

    first commit

---
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MoodAnalyserTest {
+    @Test
+    void testMoodAnalysis() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a sad message");
+        assertThat(mood, CoreMatchers.is("SAD"));
+    }
+
+    @Test
+    void HappyMoods() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a happy message");
+        assertThat(mood, CoreMatchers.is("HAPPY"));
+    }
+
+}
